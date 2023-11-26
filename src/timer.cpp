92c92e1c4aab599d24a15496ec89359b7cb4bdc1
Alessio Ballotti <alessioballotti@tiscali.it>
2023-11-26 02:43:08 +0100

    fix timer profile bug

--- src/timer.cpp
+++ src/timer.cpp
@@ -8,7 +8,7 @@ void profile::reset()
 #if defined(CPU_MIMXRT1176DVMAA)
     e = 0;
 #else
-    e = duration_type::zero();;
+    e = duration_type::zero();
 #endif
     us_min = UINT32_MAX;
     us_max = 0;
@@ -20,15 +20,16 @@ void profile::update()
 {
 #if defined(CPU_MIMXRT1176DVMAA)
     uint32_t us{ (uint32_t)((float)e * (1e6f / (float)SystemCoreClock)) };
+    e = 0;
 #else
     uint32_t us{ (uint32_t)std::chrono::duration_cast<std::chrono::microseconds>(e).count() };
+    e = duration_type::zero();
 #endif
 
     us_min = us < us_min ? us : us_min;
     us_max = us > us_max ? us : us_max;
     us_avg = (us_avg * count + (float)us) / (count + 1.f);
     count += 1.f;
-    us = 0;
 }
 
 //------------------------------------------------------------------------------
